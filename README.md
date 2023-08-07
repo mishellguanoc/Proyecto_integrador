d0c773e6d4ad727348d8b4384fbc4e7fb63c0def
mishellguanoc <ygirl12abb@gmail.com>
2023-08-07 11:22:52 -0500

    Agregar un bucle infinito que lee caracteres y sale al presionar la tecla UP

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 Proyecto Integrador parte 1
 Crear un repositorio de Github para el proyecto.
-
 Crear el archivo README.md con la descripción del proyecto.
 Crear el archivo main del proyecto
 Pedir el nombre del jugador por teclado
--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+from readchar import readkey, key
+''' la libreria readchar tiene dos metodos 
+readchar.readchar() para leer caracteres individuales
+y readchar.readkey(). para leer pulsaciones de teclas.
+
+
+'''
 jugador = input("Escribe tu nombre: ")
 print("Bienvenido " + jugador + " al juego")
+
+while True:
+    impresion = readkey()
+    print(f'{impresion}\n')
+    if impresion == key.UP:
+        break
