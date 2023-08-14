27d607bae3bd03811df9e29f9e2d4aba36810fa3
mishellguanoc <ygirl12abb@gmail.com>
2023-08-14 15:52:08 -0500

    Implementar un bucle que imprima un numero y borre la terminal si se presiona n

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from readchar import readkey, key
 ''' la libreria readchar tiene dos metodos 
 readchar.readchar() para leer caracteres individuales
@@ -8,8 +9,13 @@ y readchar.readkey(). para leer pulsaciones de teclas.
 jugador = input("Escribe tu nombre: ")
 print("Bienvenido " + jugador + " al juego")
 
-while True:
+numero = 0
+while numero <= 50:
     impresion = readkey()
-    print(f'{impresion}\n')
-    if impresion == key.UP:
-        break
+    
+    if impresion == 'n':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(numero)
+        numero += 1
+
+print("Juego terminado")
