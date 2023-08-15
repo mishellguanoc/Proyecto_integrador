8a118942be7a2d4296709194e6e66738686ffb82
mishellguanoc <ygirl12abb@gmail.com>
2023-08-15 16:48:02 -0500

    Se integro el laberinto del juego

--- main.py
+++ main.py
@@ -1,21 +1,61 @@
 import os
 from readchar import readkey, key
-''' la libreria readchar tiene dos metodos 
-readchar.readchar() para leer caracteres individuales
-y readchar.readkey(). para leer pulsaciones de teclas.
+from typing import List, Tuple
 
-
-'''
 jugador = input("Escribe tu nombre: ")
 print("Bienvenido " + jugador + " al juego")
 
-numero = 0
-while numero <= 50:
-    impresion = readkey()
-    
-    if impresion == 'n':
-        os.system('cls')
-        print(numero)
-        numero += 1
 
-print("Juego terminado")
+mapa_str = '..###################\n....#.#.............#\n###.#.###.###.#.#####\n#.#.........#.#...#.#\n#.#######.#######.#.#\n#.............#.....#\n###.###.#.###.###.###\n#...#.#.#.#.#.#.#.#.#\n#####.#.###.###.#.#.#\n#.........#.#.......#\n#.#####.###.#.#.#.###\n#.....#.#.#...#.#...#\n#########.###.#######\n#.#...#...#.#.......#\n#.#.#.#.###.#######.#\n#.#.#.....#.#...#...#\n#.###.#####.###.#.#.#\n#...#.#.#.........#.#\n###.#.#.#.#.#####.###\n#.........#.....#...#\n###################.#'
+
+def convertir_mapa(mapa_str):
+    lineas = mapa_str.strip().split('\n')  # Dividir la cadena en líneas
+    
+    global matriz
+    matriz = [list(linea) for linea in lineas]  # Crear la matriz
+
+    return matriz
+
+
+def limpiar_pantalla():
+    os.system('cls')
+limpiar_pantalla()
+
+
+def main_loop(mapa:List[List[str]],inicio:Tuple[int,int], fin: Tuple[int,int]):
+    px, py = inicio
+
+    while (px,py)!= fin:
+        limpiar_pantalla()
+        tecla = readkey()
+        nueva_px, nueva_py = px, py
+
+        if tecla == key.UP:
+            nueva_px -=1
+        elif tecla == key.DOWN:
+            nueva_px += 1
+        elif tecla == key.LEFT:
+            nueva_py -= 1
+        elif tecla == key.RIGHT:
+            nueva_py += 1
+        
+        if (
+            0 <= nueva_px < len(mapa) and
+            0<= nueva_py < len(mapa[0]) and
+            mapa[nueva_px][nueva_py] != '#'
+        ):
+            mapa[px][py] = '.'
+            px,py = nueva_px, nueva_py
+            mapa[px][py] = 'P'
+
+        limpiar_pantalla()
+    print("Has llegado a tu destino")
+
+
+
+    
+matriz = convertir_mapa(mapa_str)
+inicio = (0, 0)
+fin = (len(matriz) - 1, len(matriz[0]) - 1)
+
+main_loop(matriz, inicio, fin)
